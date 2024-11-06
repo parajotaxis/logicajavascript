86978c17995a91c36c3ecd8db73ad35339374e4d
Jefferson Xavier <67285300+parajotaxis@users.noreply.github.com>
2024-11-06 18:28:39 -0200

    fim cap 5 comparando variaveis

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 alert('Bem vindo ao jogo do número secreto');
-let nemeroSecreto = 29;
-prompt('Escolha um número entre 1 e 30');
+let nemeroSecreto = 5;
+let chute = prompt('Escolha um número entre 1 e 10');
+
+if (chute == nemeroSecreto) {
+    console.log('Isso ai! Você descobriu o número secreto (5)');
+}
