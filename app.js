c17ca74643afaee0c28e43e8d70d3489264770f4
Jefferson Xavier <67285300+parajotaxis@users.noreply.github.com>
2024-11-07 19:42:10 -0200

    fim do cap 2-3 concatenação

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 alert('Bem vindo ao jogo do número secreto');
-let numeroSecreto = 5;
+let numeroSecreto = 4;
 console.log (numeroSecreto)
 let chute = prompt('Escolha um número entre 1 e 10');
 
 // se chute for igual ao número secreto
 if (chute == numeroSecreto) {
-    alert('Isso ai! Você descobriu o número secreto (5)');
+    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}.`);
 } else {
     alert('Você errou :(');
 }
