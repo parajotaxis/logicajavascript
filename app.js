1b4abaf3d7f8793b79afd09de4936e384a2f74c7
Jefferson Xavier <parajotaxis@gmail.com>
2024-11-09 14:55:46 -0300

    fim do cap 3-2 novas condições

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 alert('Bem vindo ao jogo do número secreto');
-let numeroSecreto = 9;
+let numeroSecreto = 5;
 console.log (numeroSecreto)
 let chute = prompt('Escolha um número entre 1 e 10');
 
@@ -7,5 +7,10 @@ let chute = prompt('Escolha um número entre 1 e 10');
 if (chute == numeroSecreto) {
     alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}.`);
 } else {
-    alert('Você errou :(');
+    if (chute > numeroSecreto) {
+        alert(`O número secreto é menor que o ${chute}`);
+    } else {
+        alert(`O número secreto é maior que o ${chute}`);
+    }
+    
 }
