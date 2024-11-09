15d74353700196d2968b72d8991c0621eaf8fd4b
Jefferson Xavier <parajotaxis@gmail.com>
2024-11-09 15:27:03 -0300

    fim cap 3-4 tentativas

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 alert('Bem vindo ao jogo do número secreto');
 let numeroSecreto = 5;
-console.log (numeroSecreto)
+console.log (numeroSecreto);
 let chute;
+let tentativas = 1;
 
 // enquanto chute não for igual ao numero secreto
 while (chute != numeroSecreto) {
     chute = prompt('Escolha um número entre 1 e 10');
     // se chute for igual ao número secreto
     if (chute == numeroSecreto) {
-        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}.`);
+        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
     } else {
         if (chute > numeroSecreto) {
             alert(`O número secreto é menor que o ${chute}`);
         } else {
             alert(`O número secreto é maior que o ${chute}`);
         }
-
+        // tentativas = tentativas + 1
+        tentativas++
     }
 }
 
